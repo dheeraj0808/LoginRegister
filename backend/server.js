752042127edf4e5fe1ff953d872cc4e7b8f50df2
dheeraj0808 <dheerajsingh1939@gmail.com>
2026-02-20 12:46:53 +0530

    .

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,5 @@
-const express = require("express");
-const app = express();
+require('dotenv').config();
+const app = require('./src/app');
 
-const PORT = 5000;
-app.listen(PORT, () => console.log("✅ Server running on port", PORT));
-
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
