7b3caa6c51621991a20b27188c0675a917cec528
dheeraj0808 <dheerajsingh1939@gmail.com>
2026-03-07 01:24:41 +0530

    till the connection to db in sequilize is completed

--- backend/Server.js
+++ backend/Server.js
@@ -1,8 +1,11 @@
-require("dotenv").config({ path: "./src/.env" });
-const app = require("./src/app");
+const app = require('./src/app');
+const sequelize = require('./src/config/db');
+require('dotenv').config();
+sequelize.authenticate();
 
-const PORT = process.env.PORT || 3002;
+
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}`);
 });
--- backend/src/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const { register } = require("../controller/auth.controller");
-const router = express.Router();
-
-// POST /api/auth/register
-router.post("/register", register);
-
-module.exports = router;
